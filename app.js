f4bd98c6aaf5b2a64e89bad5c3d2945d6a6add31
TatsuyaKamohara <kamontia@gmail.com>
2018-02-05 22:05:16 +0900

    modify config

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const crypto = require("crypto");
 const fs = require('fs');
 const conf = require('./conf');
 const https = require('https');
-const googlehome = require('./google-home-notifier');
+const googlehome = require('google-home-notifier');
 
 
 // load config
@@ -27,13 +27,13 @@ var options = {
   cert: fs.readFileSync(conf.fullchain)
 }
 
-var app = express();
 
+var app = express();
 // initialization
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+  extended: false
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -42,10 +42,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 var server = https.createServer(options, app);
 
 
-console.log('Server is online.');
+console.log('Server is online. Port:' + serverPort);
+console.log("debug ::");
+console.log("lineChannelAccessToken:" + lineChannelAccessToken);
+console.log("lineChannelSecret:" + lineChannelSecret);
 
 // Validate signature
-const validate_signature = function(signature, body) {
+function validate_signature(signature, body) {
   return signature == crypto.createHmac('sha256', lineChannelSecret).update(new Buffer(JSON.stringify(body), 'utf8')).digest('base64');
 };
 
@@ -62,30 +65,36 @@ app.post('/', function(req, res) {
 
 // recieve POST message. Endpoint /webhook
 app.post('/webhook', function(req, res) {
+  console.log(req.body);
   // Validate signature
   if (!validate_signature(req.headers['x-line-signature'], req.body)) {
     console.log("Error:Validation");
     res.status(401);
     return null;
   }
+  console.log(req.body);
   var text = req.body.text;
+  console.log("text:" + text);
   if (text) {
     try {
       googlehome.notify(text, function(notifyRes) {
+        console.log("Success");
         console.log(notifyRes);
         res.send(deviceName + ' will say: ' + text + '\n');
       });
     } catch (err) {
+      console.log("Error");
       console.log(err);
       res.sendStatus(500);
       res.send(err);
     }
   } else {
+    console.log("else phase");
     res.send('Please POST "text=Hello Google Home"');
   }
-  res.writeHead(200, {
-    "Content-type": "text/plain"
-  });
+  // res.writeHead(200, {
+  //   "Content-type": "text/plain"
+  // });
   res.end();
 });
 
